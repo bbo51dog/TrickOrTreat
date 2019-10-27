a9afe19b23d99a693fe5dd12002a949d1d0a1bd1
bbo51dog <bbo51@icloud.com>
2019-10-27 21:55:31 +0900

    halloweenコマンド作成

---
+++ src/bbo51dog/trickortreat/HalloweenCommand.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace bbo51dog\trickortreat;
+
+use pocketmine\Player;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+
+class HalloweenCommand extends Command{
+    
+    public function __construct(){
+        parent::__construct("halloween", "Halloween Command", "/halloween");
+        $this->setPermission("command.halloween");
+    } 
+
+    public function execute(CommandSender $sender, string $commandLabel, array $args){
+        if(!$sender instanceof Player){
+            $sender->sendMessage("サーバー内で使用してください");
+            return;
+        }
+    }
+}
--- src/bbo51dog/trickortreat/Main.php
+++ src/bbo51dog/trickortreat/Main.php
@@ -5,5 +5,7 @@ namespace bboyyu51\trickortreat;
 use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase{
-    
+    public function onEnable(){
+        $this->getServer()->getCommandMap()->register("halloween", new HalloweenCommand());
+    }
 }
