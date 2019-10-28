8fc30acef95898adfdb20fbe403d08f0ef9140ed
bbo51dog <bbo51@icloud.com>
2019-10-28 22:06:41 +0900

    インターフェース

--- src/bbo51dog/trickortreat/api/Trick.php
+++ src/bbo51dog/trickortreat/api/Trick.php
@@ -2,6 +2,13 @@
 
 namespace bbo51dog\trickortreat\api;
 
-interface Trick{
+use pocketmine\Player;
 
+interface Trick{
+    public function blindness(Player $player): void;
+    public function hunger(Player $player): void;
+    public function poison(Player $player): void;
+    public function slowness(Player $player): void;
+    public function nausea(Player $player): void;
+    public function teleport(Player $player): void;
 }
