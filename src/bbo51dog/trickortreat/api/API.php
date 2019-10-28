1e9a2b251437b052202fd18b4d4e4907e36630c4
bbo51dog <bbo51@icloud.com>
2019-10-28 17:04:20 +0900

    interface

--- src/bbo51dog/trickortreat/api/API.php
+++ src/bbo51dog/trickortreat/api/API.php
@@ -4,4 +4,11 @@ namespace bbo51dog\trickortreat\api;
 
 interface API{
 
+    /** @var int */
+    public const TRICK = 0;
+    public const TREAT = 1;
+
+    public function select(): int;
+    public function trick(): void;
+    public function treat(): void;
 }
