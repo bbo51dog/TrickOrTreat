6642b72ca84c8a87590e8851a77530591ceb6a79
bbo51dog <bbo51@icloud.com>
2019-10-28 17:25:15 +0900

    APIInstanceに関数作成

--- src/bbo51dog/trickortreat/api/APIInstance.php
+++ src/bbo51dog/trickortreat/api/APIInstance.php
@@ -4,4 +4,19 @@ namespace bbo51dog\trickortreat\api;
 
 class APIInstance implements API{
 
+    public function select(): int{
+
+    }
+
+    public function trick(Player $player): void{
+
+    }
+
+    public function treat(Player $player): void{
+
+    }
+
+    public function run(Player $player): void{
+        
+    }
 }
